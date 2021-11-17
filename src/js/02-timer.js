027b03afc63c9cdc9369da3f8ec1a481d63612f2
pekosk <rkostyk64@gmail.com>
2021-11-17 16:34:39 +0200

    Подключил и настроил библиотеку

---
+++ src/js/02-timer.js
@@ -0,0 +1,39 @@
+// Описан в документации
+import flatpickr from 'flatpickr';
+// Дополнительный импорт стилей
+import 'flatpickr/dist/flatpickr.min.css';
+
+const inputDate = document.querySelector('#datetime-picker');
+const submitDate = document.querySelector('[data-start]');
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  minDate: "today",
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+const fp = flatpickr(inputDate, options);
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
