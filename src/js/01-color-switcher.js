87fe714c622f351d85a7939fa045ab151237191a
pekosk <rkostyk64@gmail.com>
2021-11-17 15:30:30 +0200

    1 таск

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,14 +2,28 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-const changeBtn = document.querySelector('.change-color');
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]')
 const body = document.querySelector('body');
 const colorVal = document.querySelector('.color')
 
 const clrCngClick = () => {  
+  startBtn.setAttribute('disabled', 'disabled');
+  stopBtn.removeAttribute('disabled');
+  colorBody = setInterval(cngClick, 1000);
+  return body.style.backgroundColor = getRandomHexColor();
+}
+
+const stopCngClick = () => {
+  startBtn.removeAttribute('disabled');
+  stopBtn.setAttribute('disabled', 'disabled');
+  clearInterval(colorBody);
+}
+
+const cngClick = () => {
   const bgColor = body.style.backgroundColor = getRandomHexColor();
-  colorVal.textContent = bgColor}
+}
 
-changeBtn.addEventListener('click', clrCngClick);
+startBtn.addEventListener('click', clrCngClick);
 
-setInterval(clrCngClick, 1)
+stopBtn.addEventListener('click', stopCngClick);
