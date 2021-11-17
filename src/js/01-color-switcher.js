23367b01d23677446aa26e0ffcdece229859a502
pekosk <rkostyk64@gmail.com>
2021-11-17 14:32:43 +0200

    Стили кнопок

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,15 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const changeBtn = document.querySelector('.change-color');
+const body = document.querySelector('body');
+const colorVal = document.querySelector('.color')
+
+const clrCngClick = () => {  
+  const bgColor = body.style.backgroundColor = getRandomHexColor();
+  colorVal.textContent = bgColor}
+
+changeBtn.addEventListener('click', clrCngClick);
+
+setInterval(clrCngClick, 1)
